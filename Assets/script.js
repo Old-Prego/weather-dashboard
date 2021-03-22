de1688b50b5b9c948aa3dd403c08842460e85004
Old Prego <spymasterluke@gmail.com>
2021-03-22 11:08:01 -0500

    Re-read through the requirements document and realized I was missing the ability to click on previous searches. This took a couple of different tries, but I added an onclick event to each of the list items and a function to take care of re-searching

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+// I know, quite the collection of variables right?
+// I think there is a much better way of organizing and getting these variables/looping through the forms, but I didn't spend much time on trying to find it.
 var cityHeader = document.getElementById("city");
 var dateHeader = document.getElementById("date");
 var prevCityList = document.getElementById("previousSearch");
@@ -32,23 +34,35 @@ var searches = [];
 var lat;
 var lon;
 
-const html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
-
+// Sets the initial date on the header.
 dateHeader.textContent = moment().format("MMM DD, YYYY");
 
+// This is a HTML template parsing function written by Nate.
+// It takes a string of HTML and variables, concatenating it together into usable HTML code taht can be appended to the document
+const html = (strings, ...values) => new DOMParser().parseFromString(strings.map((string, i) => strings[i] + values[i]).join(''), "text/html").body.firstChild;
 
+// This function builds the list of previous search results using the code on line 42 and displays them on the page when called. 
+// If I were to spend some more time on this, I would like to limit the amount of searches displayed/saved.
 function buildList(cityList){
 
     let listItem = html`
 
         <ul class="list-group" id="searchList">
-            ${cityList.map(city => /*html*/`<li class="list-group-item">${city}</li>`).join('')}
+            ${cityList.map(city => /*html*/`<button onclick="prevSearch(this.textContent)" class="list-group-item list-group-item-action">${city}</button>`).join('')}
         </ul>
     `
     prevCityList.removeChild(prevCityList.lastElementChild);
     prevCityList.appendChild(listItem);
 }
 
+function prevSearch(city){
+    cityHeader.textContent = city;
+    document.getElementById("cityInput").value = city;
+    forecastWeather();
+}
+
+// This code is used to convert temperatures from Kelvin to Fahrenheit
+// It is necessary since everything returned from the weather API is in Kelvin
 function KtoF(temp){
     var temp1 = temp - 273.15;
     temp1 = temp1 * 1.8;
@@ -56,6 +70,7 @@ function KtoF(temp){
     return temp1;
 }
 
+// This function loops through the returned data and populates the main dashboard and all the cards.
 function popResults(data){
     curIcon.textContent = data.daily[0].weather[0].main;
     curTemp.textContent = (Math.round(KtoF(data.daily[0].temp.day) * 10) / 10) + "\u00B0F";
@@ -89,6 +104,7 @@ function popResults(data){
     c5Hum.textContent = data.daily[5].humidity + "%";
 }
 
+// This function takes the latitude and longitude provided by the geocoding API and fetches data from the openweather API
 function geoToWeather(lat,lon){
 
     var searchCriteria = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minutely,hourly,alerts&appid=24ef92959cfdb5f639da2349846061e3"
@@ -98,16 +114,22 @@ function geoToWeather(lat,lon){
             return response.json();
         })
         .then(function(data) {
-            console.log(data);
             popResults(data);
         })
 
 }
 
+// This event triggers when the main form is submitted
+// It takes the searched city, populates it onto the main dashboard, and fetches the latitude and longitude from the geocoding API
+// It also saves that city into local storage on the saved searches list and populates the list on the screen
 function forecastWeather(event){
-    event.preventDefault();
-    var cityName = document.getElementById("cityInput").value;
+    if(typeof(event) == "undefined"){
 
+    } else {
+        event.preventDefault();
+    };
+
+    var cityName = document.getElementById("cityInput").value;
     cityHeader.textContent = cityName;
 
     savedSearches = JSON.parse(localStorage.getItem("searches"));
@@ -125,20 +147,40 @@ function forecastWeather(event){
             return response.json();
         })
         .then(function(data) {
-            console.log(data)
             lat = data.data[0].latitude;
-            console.log(lat);
             lon = data.data[0].longitude;
-            console.log(lon)
 
             geoToWeather(lat,lon);
         })
     
 }
 
-savedSearches = JSON.parse(localStorage.getItem("searches"));
-if(savedSearches !== null){
-    searches = savedSearches;
-    buildList(searches.reverse());
+// This function loads the initial screen, populating the last search result, previous search results, and loading the dashboard with the last searched result.
+function init(){
+
+    savedSearches = JSON.parse(localStorage.getItem("searches"));
+
+    if(savedSearches !== null){
+        searches = savedSearches.reverse();
+        buildList(searches);
+        document.getElementById("cityInput").value = searches[0];
+
+        var cityName = document.getElementById("cityInput").value;
+        cityHeader.textContent = cityName;
+
+        var geoSearch ="http://api.positionstack.com/v1/forward?access_key=4d0112f4606df65955616785ca11b046&query=" + cityName;
+
+        fetch(geoSearch)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                lat = data.data[0].latitude;
+                lon = data.data[0].longitude;
+
+                geoToWeather(lat,lon);
+            })
+    }
 }
 
+init();
